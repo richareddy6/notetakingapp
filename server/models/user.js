5f40ee4a18f9c7d0e4cbf1f71620256451ff1238
richareddy6 <115595934+richareddy6@users.noreply.github.com>
2022-11-18 05:19:39 -0500

    Add files via upload

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+
+
+const users=[{
+    userId:1,
+    userName:"Richa",
+    password:"richa06@"
+},
+{
+    userId:2,
+    userName:"Reddy",
+    password:"richerich14@"
+},
+{
+    userId:3,
+    userName:"Nagireddy",
+    password:"@$@&@*"
+},
+{
+    userId:4,
+    userName:"RRR",
+    password:"abcd"
+},]
+
+let getUsers=()=>users;
+
+module.exports={getUsers};
