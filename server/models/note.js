7558a947e569a4b91f7dccb55155cb9471a1957d
richareddy6 <115595934+richareddy6@users.noreply.github.com>
2022-11-18 09:56:32 -0500

    Add files via upload

---
+++ server/models/note.js
@@ -0,0 +1,16 @@
+const notes=[{
+    noteId:11,
+    noteDescription:"This is 1st note"
+},{
+    noteId:22,
+    noteDescription:"This is 2nd note"
+},{
+    noteId:33,
+    noteDescription:"This is 3rd note"
+},]
+
+
+let getNotes=()=>notes;
+
+
+module.exports={getNotes};
