085d496e1f91c560b0ce5f86753d087c8c2e2598
richareddy6 <115595934+richareddy6@users.noreply.github.com>
2022-12-06 21:23:11 -0500

    Add files via upload

--- server/routes/note.js
+++ server/routes/note.js
@@ -1,3 +1,4 @@
+/*
 const express=require('express');
 const Note=require('../models/note');
 const router=express.Router();
@@ -13,3 +14,59 @@ router.get('/',(req,res)=>{
 });
 
 module.exports=router;
+*/
+const express = require('express');
+const Note = require('../models/note');
+const router = express.Router();
+
+router
+  .get('/', async (req, res) => {
+    try {
+      const notes = await Note.getAllNotes();
+      res.send(notes);
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+
+  .post('/read', async (req, res) => {
+    try {
+      let note = await Note.read(req.body);
+      res.send({...note, password: undefined})
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+
+  /*
+  .post('/register', async (req, res) => {
+    try {
+      let user = await User.register(req.body);
+      res.send({...user, password: undefined})
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+  */
+  .put('/edit', async (req, res) => {
+    try {
+      let note = await Note.editNote(req.body);
+      res.send({...user, password: undefined});
+    } catch(err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+
+  .delete('/delete', async (req, res) => {
+    try {
+      User.deleteNote(req.body);
+      res.send({success: "We'll Miss You... :("})
+    } catch(err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+
+
+
+  
+module.exports = router;
